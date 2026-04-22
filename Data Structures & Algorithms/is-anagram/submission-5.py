cee9c3a1cfdcfc9208706dab4bf6265d23a8a2b7
TheSuperxNovaZ <superxnovaz@gmail.com>
2026-04-22 15:30:39 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,19 @@
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         uniqA={}
+#         uniqB={}
+#         for x in s:
+#             if x in uniqA:
+#                 uniqA[x] += 1
+#             else:
+#                 uniqA[x] = 1
+#         for y in t:
+#             if y in uniqB:
+#                 uniqB[y] += 1
+#             else:
+#                 uniqB[y] = 1
+#         return uniqA==uniqB
+
+class Solution:
+    def isAnagram(self, s:str,t:str)->bool:
+        return ''.join(sorted(s))==''.join(sorted(t))
