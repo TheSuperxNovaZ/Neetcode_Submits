b39940ec6a29de2a41eaed543b5c629ca68b3d6d
TheSuperxNovaZ <superxnovaz@gmail.com>
2026-09-18 16:41:34 +0530

    Add: non-cyclical-number - submission-2

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+
+        def getNext(n):
+            total = 0
+
+            while n > 0:
+                digit = n % 10
+                total += digit * digit
+                n //= 10
+
+            return total
+
+        slow = n
+        fast = getNext(n)
+
+        while fast != 1 and slow != fast:
+            slow = getNext(slow)
+            fast = getNext(getNext(fast))
+
+        return fast == 1
